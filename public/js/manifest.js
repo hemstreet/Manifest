5ec9bf1de6c24e22899fb0e6ebef7b1f101e8b68
Hemstreet <jonhemstreet@gmail.com>
2015-07-12 22:13:14 -0600

    progress with bootstrap demo template

--- public/js/manifest.js
+++ public/js/manifest.js
@@ -11,25 +11,67 @@ var _ = require('lodash');
 
             var json = require('../../config/config.json'),
                 config = _.assign( json, _config),
-                slides = json.slides;
+                $body = $('body');
+
+            this.slides = json.slides;
 
             this.$underlay = $('<div>').addClass(json.underlayClass)[0];
+            $body.append(this.$underlay);
 
-            $('body').append(this.$underlay);
+            if(json.startup === true) {
+                $body.addClass('manifest-tutorial');
+            }
 
-
-            _(slides).forEach(function(slide) {
-                $(slide.selector).addClass('manifest-active');
-                console.log($(slide.selector));
-            }).value();
+            $(this.slides[0].selector).addClass('manifest-active');
+            this.currentIndex++;
 
         },
         next: function() {
 
+            if(this.slides.length > this.currentIndex) {
+                $(this.slides[this.currentIndex].selector).removeClass('manifest-active');
+
+                var $slide = $(this.slides[this.currentIndex++].selector);
+                $slide.addClass('manifest-active');
+
+                this.scrollTo($slide);
+
+            }
+            else
+            {
+                this.endOfSlides();
+            }
+
+            this.scrollTo()
+
         },
-
         previous: function() {
+            if(this.currentIndex > 0) {
+                $(this.slides[this.currentIndex].selector).removeClass('manifest-active');
 
+                var $slide = $(this.slides[this.currentIndex--].selector);
+                $slide.addClass('manifest-active');
+
+                this.scrollTo($slide);
+            }
+            else
+            {
+                this.endOfSlides();
+            }
+
+        },
+        scrollTo : function($el) {
+
+            $('html, body').animate({
+                scrollTop: $el.offset().top
+            }, 1000);
+
+        },
+        endOfSlides: function() {
+            console.log('hit end of slides');
+        },
+        beginningOfSlides: function() {
+            console.log('beginning of slides');
         }
     };
 
